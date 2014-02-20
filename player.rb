11a64d4ccf40b07cdb2a7d0d73ddb108ef567f10
devill <devillmeister@gmail.com>
2014-02-20 21:27:19 +0100

    Handle showdown events

--- player.rb
+++ player.rb
@@ -3,4 +3,8 @@ class Player
   def bet_request(game_state)
     0
   end
+
+  def showdown(game_state)
+
+  end
 end
--- player_service.rb
+++ player_service.rb
@@ -5,5 +5,8 @@ require_relative 'player'
 post "/" do
   if params[:action] == 'bet_request'
     Player.new.bet_request(JSON.parse(params[:game_state])).to_s
+  elsif params[:action] == 'showdown'
+    Player.new.showdown(JSON.parse(params[:game_state]))
+    'OK'
   end
 end

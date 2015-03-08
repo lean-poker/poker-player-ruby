266f1d47aa184d5f96ba60605818317df1db68f7
devill <devillmeister@gmail.com>
2015-03-08 21:43:03 +0100

    Remove scripts used in the past

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-bundle exec ruby player_service.rb &
-
-echo $! > sinatra.pid
--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-kill `cat sinatra.pid`
-rm sinatra.pid
-
